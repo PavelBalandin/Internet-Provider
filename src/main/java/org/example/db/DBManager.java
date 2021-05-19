5f299e04d5deefe9b3be93dcf90fc63a3c0eef94
PashaBalandin <pashabalandin1200@gmail.com>
2021-05-19 13:20:37 +0300

    DBmanager has been added

---
+++ src/main/java/org/example/db/DBManager.java
@@ -0,0 +1,30 @@
+package org.example.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class DBManager {
+
+    private static DBManager instance;
+
+    private DBManager() {
+    }
+
+    public static synchronized DBManager getInstance() {
+        if (instance == null)
+            instance = new DBManager();
+        return instance;
+    }
+
+    public Connection getConnection(String url) {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url);
+        } catch (SQLException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return connection;
+    }
+
+}
